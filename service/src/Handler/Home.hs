daa61ad6eeed507c2ff15d0b9989e0e3025098a0
biglidio <lucas-fj@hotmail.com>
2018-06-13 09:38:20 -0300

    Definição de 4 tabelas + correção

--- service/src/Handler/Home.hs
+++ service/src/Handler/Home.hs
@@ -6,13 +6,8 @@
 module Handler.Home where
 
 import Import
-<<<<<<< HEAD
 -- import Network.HTTP.Types.Status
 -- import Database.Persist.Postgresql
-=======
-import Network.HTTP.Types.Status
-import Database.Persist.Postgresql
->>>>>>> 92206931f96c95b008e7429459d4db29cce0116a
 
 getHomeR :: Handler Html
 getHomeR = undefined
